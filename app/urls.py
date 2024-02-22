74f4adc43298d6ea2fb5728f3b6ea3c791e73cff
castilho <guilherme.castilhot2c18@gmail.com>
2024-02-22 19:24:56 -0300

    detalhe de genero

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_view, genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("genres/", genre_view, name="genre")
+    path("genres/", genre_view, name="genre"),
+    path("genres/<int:pk>/", genre_detail_view, name="genre-detail-view")
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 
@@ -19,3 +20,10 @@ def genre_view(request):
         new_genre.save()
 
         return JsonResponse({"id": new_genre.id, "name": new_genre.name}, status=201)
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {"id": genre.id, "name": genre.name}
+    return JsonResponse(data)
