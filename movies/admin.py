111db6672a77894012617d327074e578bd13ef00
castilho <guilherme.castilhot2c18@gmail.com>
2024-02-29 17:32:58 -0300

    admin de movies

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from movies.models import Movie
+    
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "genre", "release_date", "resume")
--- movies/models.py
+++ movies/models.py
@@ -5,7 +5,7 @@ from actors.models import Actor
 
 class Movie(models.Model):
     title = models.CharField(max_length=500)
-    genres = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name="movies")
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name="movies")
     release_date = models.DateField(null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name="movies") # um filme pode ter varios atores ligados nele
     resume = models.TextField(null=True, blank=True)
