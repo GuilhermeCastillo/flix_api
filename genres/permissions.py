c67d9db21ce37d777f3a0681314af85f022d1bd8
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-02 20:58:25 -0300

    permissions global class

--- genres/permissions.py
+++ genres/permissions.py
@@ -11,4 +11,10 @@ class GenrePermissionClass(permissions.BasePermission):
         if request.method == "POST":
             return request.user.has_perm("genres.add_genre")
 
+        if request.method in ["PATCH", "PUT"]:
+            return request.user.has_perm("genres.change_genre")
+
+        if request.method == "DELETE":
+            return request.user.has_perm("genres.delete_genre")
+
         return False
