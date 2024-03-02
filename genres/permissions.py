21d2c37113ace4d80b742f77816f3ad6d1b0a663
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-02 20:10:24 -0300

    permissions Genres

---
+++ genres/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class GenrePermissionClass(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+
+        if request.method in ["GET", "OPTIONS", "HEAD"]:
+            return request.user.has_perm("genres.view_genre")
+
+        if request.method == "POST":
+            return request.user.has_perm("genres.add_genre")
+
+        return False
--- genres/views.py
+++ genres/views.py
@@ -1,15 +1,16 @@
 from genres.models import Genre
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from genres.permissions import GenrePermissionClass
 from genres.serializers import GenreModelSerializer
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass, GenrePermissionClass)
     queryset = Genre.objects.all()
     serializer_class = GenreModelSerializer
 
