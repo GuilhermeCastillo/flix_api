1ee9c4cfd83652584ede6458f5ed75f9c705c295
castilho <guilherme.castilhot2c18@gmail.com>
2024-03-01 21:00:56 -0300

    verify e refresh token added

--- actors/views.py
+++ actors/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from actors.models import Actor
 from actors.serializers import ActorModelSerializer
 
+
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorModelSerializer
 
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorModelSerializer
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from reviews.models import Review
 from reviews.serializers import ReviewModelSerializer
 
 class ReviewCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
     
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewModelSerializer
